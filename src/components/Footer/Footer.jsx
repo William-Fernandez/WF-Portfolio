362c5d5280d18196c9f868f71f3d7ffe7f8547f5
William-Fernandez <191409823+William-Fernandez@users.noreply.github.com>
2026-04-03 12:26:44 +0100

    uninstalled flowbite and removed it from the project, so I deleted the imports in index.css and updated the package.json and package-lock.json files to remove flowbite as a dependency.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full text-center text-xs md:text-sm text-tx-neutral4/80 py-6 border-t border-tx-neutral4/30 rounded-t-lg mt-10">
-            <p>© 2025 William Fernandez. All rights reserved.</p>
+            <p>© 2026 William Fernandez. All rights reserved.</p>
             <p className="mt-1">
                 Logos and trademarks belong to their respective owners.
             </p>
